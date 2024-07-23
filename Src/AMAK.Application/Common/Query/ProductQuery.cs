e9d7da2186e3a5194f405eb5b481289da9f283dc
vanthang240803 <nguyenthang240803@gmail.com>
2024-07-23 14:05:55 +0700

    Update Filter Product

--- Src/AMAK.Application/Common/Query/ProductQuery.cs
+++ Src/AMAK.Application/Common/Query/ProductQuery.cs
@@ -1,8 +1,12 @@
+using AMAK.Application.Common.Constants;
 
-namespace AMAK.Application.Common.Query
-{
-    public class ProductQuery : BaseQuery
-    {
-        
+namespace AMAK.Application.Common.Query {
+    public class ProductQuery : BaseQuery {
+        public string? Name { get; set; }
+        public string? Category { get; set; }
+        public string? Action { get; set; }
+        public string? SortBy { get; set; }
+        public string? OrderBy { get; set; }
     }
+
 }
