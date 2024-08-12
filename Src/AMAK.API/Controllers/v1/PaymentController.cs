a2841c5f00e0165adb4454990f2286f5bcff1c7f
vanthang240803 <nguyenthang240803@gmail.com>
2024-08-12 09:17:53 +0700

    Add Handler Payment Momo Request

--- Src/AMAK.API/Controllers/v1/PaymentController.cs
+++ Src/AMAK.API/Controllers/v1/PaymentController.cs
@@ -1,5 +1,6 @@
 using AMAK.Application.Services.Order.Dtos;
 using AMAK.Application.Services.Payment.Commands.Momo;
+using AMAK.Application.Services.Payment.Dtos;
 using Asp.Versioning;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
@@ -20,6 +21,14 @@ namespace AMAK.API.Controllers.v1 {
         [Route("Momo")]
         public async Task<IActionResult> Save([FromBody] CreateOrderRequest orderRequest) {
             return StatusCode(StatusCodes.Status201Created, await _mediator.Send(new PaymentMomoCommand(User, orderRequest)));
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("Momo/Handler")]
+
+        public async Task<IActionResult> HandlerMomo([FromBody] HandlerMomoRequest request) {
+            return Ok(await _mediator.Send(new HandlerMomoRequestCommand(request)));
         }
     }
 }
--- Src/AMAK.Application/Services/Payment/Commands/Momo/HandlerMomoRequestCommandHandler.cs
+++ Src/AMAK.Application/Services/Payment/Commands/Momo/HandlerMomoRequestCommandHandler.cs
@@ -2,63 +2,86 @@ using AMAK.Application.Common.Exceptions;
 using AMAK.Application.Common.Helpers;
 using AMAK.Application.Interfaces;
 using AMAK.Application.Providers.Mail;
-using AMAK.Application.Services.Order.Dtos;
+using AMAK.Application.Services.Notification;
+using AMAK.Application.Services.Notification.Dtos;
 using AMAK.Domain.Enums;
 using AMAK.Domain.Models;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using System.Net;
 
 namespace AMAK.Application.Services.Payment.Commands.Momo {
     public class HandlerMomoRequestCommandHandler : IRequestHandler<HandlerMomoRequestCommand, Response<string>> {
 
         private readonly IRepository<Domain.Models.Order> _orderRepository;
 
-        private readonly IRepository<Domain.Models.OrderDetail> _orderDetailRepository;
+        private readonly IRepository<OrderDetail> _orderDetailRepository;
         private readonly IRepository<Option> _optionRepository;
 
-        private readonly IRepository<Domain.Models.Notification> _notificationRepository;
+        private readonly INotificationService _notificationService;
 
         private readonly IMailService _mailService;
 
 
-        public HandlerMomoRequestCommandHandler(IRepository<Domain.Models.Order> orderRepository, IRepository<Domain.Models.Notification> notificationRepository, IMailService mailService, IRepository<Domain.Models.OrderDetail> orderDetailRepository, IRepository<Option> optionRepository) {
+        public HandlerMomoRequestCommandHandler(IRepository<Domain.Models.Order> orderRepository, IMailService mailService, IRepository<OrderDetail> orderDetailRepository, IRepository<Option> optionRepository, INotificationService notificationService) {
             _orderRepository = orderRepository;
-            _notificationRepository = notificationRepository;
             _mailService = mailService;
             _orderDetailRepository = orderDetailRepository;
             _optionRepository = optionRepository;
+            _notificationService = notificationService;
         }
 
         public async Task<Response<string>> Handle(HandlerMomoRequestCommand request, CancellationToken cancellationToken) {
 
-            var existingOrder = await _orderRepository.GetById(request.Data.OrderId) ?? throw new NotFoundException("Order not found!");
+            var existingOrder = await _orderRepository.GetById(request.Data.OrderId)
+            ?? throw new NotFoundException("Order not found!");
 
-            var orderDetails = await _orderDetailRepository.GetAll().Where(x => x.OrderId == existingOrder.Id).ToListAsync(cancellationToken: cancellationToken);
+            var orderDetails = await _orderDetailRepository.GetAll()
+                .Where(x => x.OrderId == existingOrder.Id)
+                .ToListAsync(cancellationToken: cancellationToken);
 
-            if (request.Data.ResultCode == EMomoResultCode.Error) {
-                try {
-                    await _orderRepository.BeginTransactionAsync();
+            switch (request.Data.ResultCode) {
+                case EMomoResultCode.Error:
+                    try {
+                        await _orderRepository.BeginTransactionAsync();
 
-                    foreach (var detail in orderDetails) {
-                        var existingOption = await _optionRepository.GetById(detail.OptionId) ?? throw new NotFoundException("Option not found");
+                        foreach (var detail in orderDetails) {
+                            var existingOption = await _optionRepository.GetById(detail.OptionId)
+                                ?? throw new NotFoundException("Option not found!");
 
-                        existingOption.Quantity += detail.Quantity;
+                            existingOption.Quantity += detail.Quantity;
 
-                        await _optionRepository.SaveChangesAsync();
+                            await _optionRepository.SaveChangesAsync();
+                        }
+
+                        _orderRepository.Remove(existingOrder);
+
+                        await _orderRepository.SaveChangesAsync();
+                        await _orderRepository.CommitTransactionAsync();
+                    } catch (Exception) {
+                        await _orderRepository.RollbackTransactionAsync();
+                        throw new BadRequestException("Query wrong!");
                     }
+                    break;
 
-                    existingOrder.Status = EOrderStatus.CANCEL;
+                case EMomoResultCode.Success:
+                    var confirmNotification = new CreateNotificationForAccountRequest() {
+                        Content = $"<p>Đơn hàng <b style=\"color: #16a34a;\">{existingOrder.Id}</b> đã được đặt thành công!</p>",
+                        Url = $"/orders/{existingOrder.Id}",
+                        IsGlobal = false,
+                        UserId = existingOrder.UserId!
+                    };
 
-                    await _orderRepository.SaveChangesAsync();
-                    await _orderRepository.CommitTransactionAsync();
+                    await _notificationService.CreateNotification(confirmNotification);
+                    await _mailService.SendOrderMail(existingOrder.Email, "Xác nhận đơn hàng", existingOrder, orderDetails);
+                    break;
 
-                } catch (Exception) {
-                    await _orderRepository.RollbackTransactionAsync();
-                    throw new BadRequestException("Query wrong!");
-                }
+                default:
+                    throw new BadRequestException("Invalid ResultCode !");
             }
 
-            throw new NotImplementedException();
+            return new Response<string>(HttpStatusCode.OK, "Handler Order Successfully!");
+
         }
     }
 }
