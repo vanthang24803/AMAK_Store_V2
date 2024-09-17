927294a51321600f44c00cd16b2353787474baa6
vanthang240803 <nguyenthang240803@gmail.com>
2024-09-17 10:46:50 +0700

    Fix Cart Router

--- Src/AMAK.Application/Services/Cart/ICartService.cs
+++ Src/AMAK.Application/Services/Cart/ICartService.cs
@@ -2,13 +2,12 @@ using System.Security.Claims;
 using AMAK.Application.Common.Helpers;
 using AMAK.Application.Services.Cart.Dtos;
 
-namespace AMAK.Application.Services.Cart
-{
-    public interface ICartService
-    {
-        Task<Response<string>> AddToCartAsync(ClaimsPrincipal claims , CartRequest request);
-        Task<Response<string>> RemoveToCartAsync(ClaimsPrincipal claims , CartRequest request);
-        Task<Response<string>> ClearCacheAsync(ClaimsPrincipal claims);
+namespace AMAK.Application.Services.Cart {
+    public interface ICartService {
+        Task<Response<string>> AddToCartAsync(ClaimsPrincipal claims, CartRequest request);
+        Task<Response<string>> RemoveToCartAsync(ClaimsPrincipal claims, CartRequest request);
+        Task<Response<string>> ClearCartAsync(ClaimsPrincipal claims);
+        Task<Response<string>> RemoveOptionsAsync(ClaimsPrincipal claims, CartRequest request);
         Task<Response<List<CartResponse>>> GetCartAsync(ClaimsPrincipal claims);
     }
 }
